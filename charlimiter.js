b7b12e144179c161ffccdfa4cad2b3868417ac93
Barry Paul <Barry@bajabook.local>
2010-05-13 14:04:36 -0700

    Fix some initialization errors for options

--- charlimiter.js
+++ charlimiter.js
@@ -8,65 +8,69 @@ CharLimiter.prototype = {
   // creation
   initialize: function(field, counter, options) {
     this.options = {
-      'maxLimit':	140,
-      'limited'	:	false
+      'maxLimit':       140,
+      'limited' :       false
       }
     Object.extend(this.options, options || {});
 
     this.fieldElem = $(field);
     this.counterElem = $(counter);
-    
+
     // register to monitor changes
-    CharLimiter.register_events();
-    
+    this.register_events();
+
     // make sure the counter is setup
     this.doCharCounter();
   },
-  
+
   register_events: function(){
     if (this.fieldElem) {
-      Event.observe(this.fieldElem, 'keyup', this.doCharCounter.bind);
-      Event.observe(this.fieldElem, 'keydown', this.doCharCounter.bind);
-      
+      Event.observe(this.fieldElem, 'keyup', this.doCharCounter.bindAsEventListener(this));
+      Event.observe(this.fieldElem, 'keydown', this.doCharCounter.bindAsEventListener(this));
+
     }
   },
-  
-  doCharCounter(event) {
+
+  doCharCounter: function(event) {
     // if there is no counter insert one
     if (!this.counterElem){
-      this.counterElem = this.fieldElem.insert({after: '<div class="char_counter"></div>'});
+      this.fieldElem.insert({after: '<div id="' + this.fieldElem.id + '_counter"></div>'});
+      this.counterElem = $(this.fieldElem.id + '_counter');
     }
-    
-    if(this.fieldElem.length >= this.options.maxLimit){
+
+    if($F(this.fieldElem).length >= this.options.maxLimit){
       // if limited then chop text at max
-      if(this.options.limited) {    
-        this.fieldElem.value = this.fieldElem.substring(0, this.options.maxLimit); 
+      if(this.options.limited) {
+        this.fieldElem.value = $F(this.fieldElem).substring(0, this.options.maxLimit);
       }
 
       // set some css classes for visual feedback
       this.counterElem.addClassName('charcount-limit');
       this.counterElem.removeClassName('charcount-safe');
-    } 
+    }
     else {
       this.counterElem.removeClassName('charcount-limit');
       this.counterElem.addClassName('charcount-safe');
     }
-    counterElem.update(this.fieldElem.length + '/' + this.options.maxLimit );
+    this.counterElem.update($F(this.fieldElem).length + '/' + this.options.maxLimit );
   }
 }
 
 
 // auto count textareas with our class
 Event.observe(window, 'load', function() {
-  
+
   // find each text area with the class "char_limiter" and add the limiter
   $$('textarea.char_limiter').each(function(textArea) {
-    CharLimiter.new(textArea, textArea.counter,
-                    {
-                      maxLimit: textArea.max,
-                      limit: textArea.limit
-                    });
+      opts = {};
+      if (textArea.hasAttribute('max')) {
+        opts.maxLimit = textArea.readAttribute('max');
+      }
+      if (textArea.hasAttribute('limit')) {
+        opts.limited = textArea.readAttribute('limit');
+      }
+      new CharLimiter(textArea, textArea.readAttribute('counter'), opts);
                                    });
-              
-              
+
+
               });
